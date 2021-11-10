63636eb621d364b77fd98abfe73319c7c3880b25
Sitgreave <andrey.chernyhov4@gmail.com>
2021-11-10 16:46:46 +0600

    Add cell creating system

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(LayoutElement))]
+public class Cell : MonoBehaviour
+{
+    private Sprite _cardSprite;
+}
---
+++ Assets/Scripts/CellCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GridLayoutGroup))]
+public class CellCreator : MonoBehaviour
+{
+    [SerializeField] private Transform _gridLayoutTransform;
+    private const int _cellsInRow = 3;
+    public void Create(Cell cell, int cellCount)
+    {
+        if (cellCount % _cellsInRow == 0) // кратно ли введёное значение минимальному количеству строк (3)
+        {
+            for (int i = 0; i < cellCount; i++)
+            {
+                Instantiate(cell, _gridLayoutTransform);
+            }
+        }
+        else Debug.Log("Incorrect cell count (not divisible by 3) ");
+    }
+
+    
+
+
+}
